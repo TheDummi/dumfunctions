24b70e65af59a7f3839ba24438551d51a69601c3
Dummi <19182@leerling.gitokbovenbouw.be>
2022-07-23 13:24:34 +0200

    1.0.4 - Log styles for vsc

--- main.js
+++ main.js
@@ -125,6 +125,25 @@ function toPhraseCase(string, lowercase = false) {
     return string?.toString()?.split(/[!?.]/gi).map(phrase => phrase?.toString().charAt(0)?.toUpperCase() + phrase?.toString()?.slice(1)).join(" ");
 }
 
+function logStyle(text, type, color) {
+    if (type.toLowerCase() == "text") {
+        if (color.toLowerCase() == "red") return `\u001b[0;31m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "green") return `\u001b[0; 32m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "yellow") return `\u001b[0; 33m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "blue") return `\u001b[0; 34m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "purple") return `\u001b[0; 35m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "cyan") return `\u001b[0; 36m${text}\u001b[0;0m`;
+    }
+
+    if (type.toLowerCase() == `background`) {
+        if (color.toLowerCase() == "red") return `\u001b[0;41m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "green") return `\u001b[0; 42m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "yellow") return `\u001b[0; 43m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "blue") return `\u001b[0; 44m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "purple") return `\u001b[0; 45m${text}\u001b[0;0m`;
+        if (color.toLowerCase() == "cyan") return `\u001b[0; 46m${text}\u001b[0;0m`;
+    }
+}
 
 module.exports = {
     // randoms
@@ -141,6 +160,8 @@ module.exports = {
     // Strings
     toPhraseCase: toPhraseCase,
     toTitleCase: toTitleCase,
-    toStringCase: toStringCase
+    toStringCase: toStringCase,
+
+    logStyle: logStyle
 
 };
